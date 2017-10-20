30d0e5f76a985fd3212dbaafb3ecaeeb2bb9f0f6
user <PROTECTED>
2017-10-20 11:21:17 -0300

    mux2_tristate

---
+++ mux2_tristate/mux2_tristate.vhd
@@ -0,0 +1,28 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.all;
+
+entity mux2_tristate is
+	port(d0, d1: in  STD_LOGIC_VECTOR(3 downto 0);
+		 s:		 in  STD_LOGIC;
+		 y:		 out STD_LOGIC_VECTOR(3 downto 0));
+end;
+
+architecture struct of mux2_tristate is
+component tristate
+	port(a:  in  STD_LOGIC_VECTOR(3 downto 0);
+		 en: in  STD_LOGIC;
+		 y:  out STD_LOGIC_VECTOR(3 downto 0));
+end component;
+
+component inverter
+	port (a: in  STD_LOGIC;
+		  y: out STD_LOGIC);
+end component;
+
+signal notS: STD_LOGIC;
+
+begin
+	sInverter: 		inverter port map(s, notS);
+	topTristate:    tristate port map(d0, notS, y);
+	bottomTristate: tristate port map(d1, s, y);
+end;
